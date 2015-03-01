e18eaa2d57e8271799060d164ae14a39ee557920
tywins <paulina1039@hotmail.com>
2015-02-28 18:47:12 -0600

    conditionalElif.py
    Mastery 17.

---
+++ conditionalElif.py
@@ -0,0 +1,10 @@
+name = str(input("Enter your name: "))
+
+if (name == "Paulina"):
+    print ("Welcome, Paulina!")
+elif (name == "Mitch"):
+    print ("Hi there, Mitch!")
+elif (name == "Kevin"):
+    print ("Hey Kev!")
+else:
+    print ("You're not welcome mate, sorry.")
