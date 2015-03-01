db6bfbcce8e8021f3e57768f7fbaac0c62282b94
tywins <paulina1039@hotmail.com>
2015-02-28 18:44:16 -0600

    commentsInPython.py
    Mastery 07.

---
+++ commentsInPython.py
@@ -0,0 +1,9 @@
+# This is how you use comments in python.
+# Anything after the # is ignored by python.
+
+print ("I could have any code like this here.") # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# print "This won't run at all."
+
+print ("This will run.")
