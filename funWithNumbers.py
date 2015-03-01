a3ce0f0400eeafa9e49cf203682f1a8d9402351b
tywins <paulina1039@hotmail.com>
2015-02-28 18:39:10 -0600

    funWithNumbers.py
    WSQ03

---
+++ funWithNumbers.py
@@ -0,0 +1,12 @@
+x=int(input("Enter a number: "))
+y=int(input("Enter another number: "))
+summ=x+y
+difference=x-y
+product=x*y
+division=x/y
+remainder=x%y
+print ("the sum of ", x, " + ", y, " = ", summ)
+print ("the difference of ", x, " - ", y, " = ", difference)
+print ("the product of ", x, " x ", y, " = ", product)
+print ("the division of ", x, " / ", y, " = ", division)
+print ("the remainder of ", x, " % ", y, " = ", remainder)
