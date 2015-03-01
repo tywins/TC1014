d730e83eb3dd125dd068b7f1fb292f939ba0010d
tywins <paulina1039@hotmail.com>
2015-02-28 18:45:28 -0600

    conditionalIf.py
    Mastery 15.

---
+++ conditionalIf.py
@@ -0,0 +1,9 @@
+# This is an example of an if-else statement in which you log in with
+# the username "graham" and the password's "abc123"
+un = str(input("Enter username: "))
+pw = int (input("Enter password: "))
+
+    if (un == abc123):
+        print ("Welcome back!")
+    else:
+        print ("Something went wrong, check that your username and password are correct.")
