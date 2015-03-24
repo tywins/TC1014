6f93c4769e79120973cdd0283094aea65e28558b
tywins <paulina1039@hotmail.com>
2015-03-23 19:36:14 -0600

    Quiz 8
    Quiz 8

--- triangle.py
+++ triangle.py
@@ -1,11 +1,12 @@
+#Paulina Romo Villalobos 
 def triangles(size):
   for i in range(1, size +1):
-    for c in range(i):
+    for x in range(i):
       print("T", end="")
     print()
 
   for i in range(1, size):
-    for c in range(size-i):
+    for x in range(size-i):
       print("T", end="")
     print()
 
