b28a7f7f61b0310e05d1b83cb214744b239a9281
tywins <paulina1039@hotmail.com>
2015-04-03 12:43:53 -0600

    Strings
    Mastery 26

---
+++ strings.py
@@ -0,0 +1,14 @@
+a = "- Yes but you didn't draw Machamp this is not what Machamp looks like."
+b = "- That is Machamp, it has 6 arms. the ONLY pókemon with 6 arms..."
+c = "(Machamp has 4 arms)"
+d = "- Well, I don't know ALL the details!"
+e = "- Okay, well you need to know SOMETHING so I can play this game."
+
+print ()
+print (b)
+print (a)
+print (d)
+print (e)
+print ()
+print (c)
+print ()
