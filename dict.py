cc41c2a61225326081bad0b806f4368e56e8264c
tywins <paulina1039@hotmail.com>
2015-04-03 12:48:50 -0600

    Dictionary
    Mastery 27

---
+++ dict.py
@@ -0,0 +1,8 @@
+dict = {'Name': 'Paulina', 'Age': 19, 'Nationality': 'Mexican'}
+
+dict['Age'] = 20 # update existing entry
+dict['Career'] = "LAD" # Add new entry
+
+print (dict)
+print ("Age: ", dict['Age'])
+print ("Career: ", dict['Career'])
