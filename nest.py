d45acc3052bd2b6f6c2d09d9746de71a64857897
tywins <paulina1039@hotmail.com>
2015-04-03 12:35:48 -0600

    Nesting conditionals
    Mastery 18

---
+++ nest.py
@@ -0,0 +1,10 @@
+pizza = str(input("Do you want pizza, y/n? "))
+
+if (pizza == "y"):
+    flav = str(input("Pepperoni (p) or Hawaiian (h): "))
+    if (flav == "p"):
+        print ("You actually suck.")
+    else:
+        print ("We should be friends.")
+else:
+  print ("u ok?")
