9995567beee72b8064b87d3f9a657b785a59b4ba
tywins <paulina1039@hotmail.com>
2015-04-03 12:53:59 -0600

    Variables
    Mastery 09

---
+++ variables.py
@@ -0,0 +1,15 @@
+i = 45        # Integer
+f = 450.6     # Floating point
+s = "Aye lil' mama lemme whisper in ya ear"     # String
+l = ['abc', 321 , 7.11, 'rad']     #List
+t = ('abc', 321 , 7.11, 'rad')     #Tuple
+
+dict = {}     #Dictionary
+dict['apple'] = "Apples are fabulous."
+
+print (i)
+print (f)
+print (s)
+print (l)
+print (t)
+print (dict['apple'])
